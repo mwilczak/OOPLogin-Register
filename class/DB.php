931028376736409b99a765577d735d70a99e594f
Mateusz Wilczak <mwilczak88@gmail.com>
2017-04-09 19:20:10 +0200

    added files classes

---
+++ class/DB.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 09.04.17
+ * Time: 19:09
+ */
+class DB
+{
+
+}
---
+++ class/Validation.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mateusz
+ * Date: 09.04.17
+ * Time: 19:13
+ */
+class Validation
+{
+
+}
